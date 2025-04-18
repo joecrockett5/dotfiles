fb061b316fd66cc6fde91e8d35464d66235edd7c
Joe Crockett <joe@crockett.net>
2025-04-18 10:10:14 +0100

    PENDING CHANGES
    Upgrading setup

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,16 +1,14 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 23
+config.font_size = 15
 
--- further config here
 config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
+config.window_decorations = "TITLE | RESIZE"
+
 config.color_scheme = "Catppuccin Frappe"
 
 config.window_background_opacity = 0.9
-config.macos_window_background_blur = 10
 
 return config
